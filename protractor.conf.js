1b99416a86656b7a10041048cde58d0f63e5dfa9
lkoshy <lkoshy@users.noreply.github.com>
2017-09-18 11:24:19 +0300

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
-      'args': [ '--window-size=800x600' ]
+      'args': ['--window-size=1690,1000']
     }
   },
 
